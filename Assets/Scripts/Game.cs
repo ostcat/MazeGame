db615ed23de14e3ada381148a7a5c899ea3a7bd6
Ekaterina Ostreva <lyapka_07@mail.ru>
2025-11-03 17:43:27 +0300

    ScriptChanges

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,13 +6,18 @@ public class Game : MonoBehaviour
     [SerializeField] private Exit _exit;
 
     private string _winMessage = "Вы выйграли, поздравляем!";
+    private bool _isRunning = true;
 
     private void Update()
     {
+        if (_isRunning == false)
+            return;
+
         if (_exit.IsExitFounded == true)
         {
             Debug.Log(_winMessage);
             _ball.StopBall();
+            _isRunning = false;
         }
     }
 }
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -5,17 +5,39 @@ public class Rotator : MonoBehaviour
     private const string HorizontalAxis = "Horizontal";
     private const string VerticalAxis = "Vertical";
 
-    [SerializeField] private float _rotationForce = 10f;
+    [SerializeField] private float _rotationSpeed = 50f;
 
+    private Rigidbody _rigidbody;
     private float _xInput;
     private float _yInput;
+    private float _xRotation;
+    private float _yRotation;
+    private Quaternion _targetRotation;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _targetRotation = transform.rotation;
+    }
 
     private void Update()
     {
         _xInput = Input.GetAxisRaw(HorizontalAxis);
         _yInput = Input.GetAxisRaw(VerticalAxis);
+    }
 
-        transform.Rotate(Vector3.right, _yInput * _rotationForce, Space.World);
-        transform.Rotate(Vector3.back, _xInput * _rotationForce, Space.World);
+    private void FixedUpdate()
+    {
+        _xRotation = _xInput * _rotationSpeed * Time.fixedDeltaTime;
+        _yRotation = _yInput * _rotationSpeed * Time.fixedDeltaTime;
+
+        Quaternion rotationXQuat = Quaternion.Euler(0, 0, -_xRotation);
+        Quaternion rotationYQuat = Quaternion.Euler(_yRotation, 0, 0);
+
+        _targetRotation = rotationXQuat * rotationYQuat * _targetRotation;
+
+        _rigidbody.MoveRotation(_targetRotation);
+
+        //transform.Rotate(Vector3.back, _xInput * _rotationForce, Space.World);
     }
 }
